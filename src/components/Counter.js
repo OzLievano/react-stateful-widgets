2fc33be80f1da0f6372353624119217a08537fd1
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-09-16 21:44:39 -0400

    added ternary op for color, added interpolation for count in div count

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -51,6 +51,8 @@ import React, { useState } from 'react'; /* STEP 0 */
 export default function Counter() {
   /* STEP 1 */
 
+  const [count, setCount] = useState(0)
+
   const increment = () => {
     /* STEP 4 */
   };
@@ -64,14 +66,14 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: count % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is even {/* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
@@ -81,3 +83,4 @@ export default function Counter() {
     </div>
   );
 }
+
